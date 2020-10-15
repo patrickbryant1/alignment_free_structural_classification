013abdafe05e964aeb5e331d4b22635a0af5b2ed
Patrick <pbryant@ae18a.scilifelab.se>
2020-10-15 17:10:04 +0200

    eda

--- eda.py
+++ eda.py
@@ -5,15 +5,15 @@
 import argparse
 import sys
 import numpy as np
-
+import pandas as pd
 import pdb
 #Arguments for argparse module:
 parser = argparse.ArgumentParser(description = '''Parse and analyze CATH data .''')
 
-parser.add_argument('sequences', nargs=1, type= str,
+parser.add_argument('--sequences', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to clustered CATH sequences.')
 
-parser.add_argument('outdir', nargs=1, type= str,
+parser.add_argument('--outdir', nargs=1, type= str,
                   default=sys.stdin, help = 'Path to output directory. Include /in end')
 
 
@@ -30,12 +30,24 @@ def read_fasta(sequences):
             if line[0]=='>':
                 #Create a new entry
                 sequence = ''
+                if len(fetched_uids)>0:
+                    fetched_sequences.append(sequence)
+                #Save uid
                 fetched_uids.append(line.split('|')[2].split('/')[0])
-                pdb.set_trace()
+
+
             else:
                 sequence+=line
 
+        #Append the last sequence
+        fetched_sequences.append(sequence)
 
+
+    df = pd.DataFrame()
+    df['uid'] = fetched_uids
+    df['sequence'] =fetched_sequences
+    df.to_csv('seqdf.csv')
+    pdb.set_trace()
 #####MAIN#####
 args = parser.parse_args()
 sequences = args.sequences[0]
---
+++ eda.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./eda.py --sequences cath-domain-seqs-S35.fa --outdir ./
