449f687bdab86ca89d6e5a0b8af98f7d382640f1
Patrick <patrick.bryant@live.com>
2020-10-19 14:00:51 +0200

    decided on 95 %

--- data/eda.sh
+++ data/eda.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 SEQUENCES=./cath-domain-seqs-S95.fa
-DOMAINLIST=./cath-domain-list-S100.txt
+DOMAINLIST=./cath-domain-list.txt
 OUTDIR=../results/
 ./eda.py --sequences $SEQUENCES --domain_list $DOMAINLIST --outdir $OUTDIR
