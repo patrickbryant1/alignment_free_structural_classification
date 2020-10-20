42614a533ca93b00c03b283050d2c96571c51ddd
Patrick <pbryant@ae18a.scilifelab.se>
2020-10-20 15:57:38 +0200

    running 200 epochs

--- model/model.py
+++ model/model.py
@@ -58,41 +58,7 @@ def read_net_params(params_file):
 
     return net_params
 
-#
-# def get_batch(grouped_labels,encoded_seqs,batch_size,s="train"):
-#     """
-#     Create batch of n pairs
-#     """
-#
-#     random_numbers = np.random.choice(len(grouped_labels),size=(batch_size*2,),replace=False) #without replacement
-#
-#     #initialize vector for the targets
-#     targets=[]
-#     s1 = []
-#     s2 = []
-#     #Get batch data - make half from the same H-group and half from different
-#     for i in range(batch_size):
-#         matches = grouped_labels[random_numbers[i]]
-#         #See if match or not
-#         if np.random.randint(2)==0: #If match add from same H-group
-#             if matches.shape[0]>1: #See if theere is more than one sequence in the H-group
-#                 pick = np.random.choice(matches,size=2, replace=False)
-#                 s1.append(np.eye(21)[encoded_seqs[pick[0]]])
-#                 s2.append(np.eye(21)[encoded_seqs[pick[1]]])
-#             else:
-#                 s1.append(np.eye(21)[encoded_seqs[matches[0]]])
-#                 s2.append(np.eye(21)[encoded_seqs[matches[0]]])
-#
-#             targets.append(1)
-#         else: #If not match add from different H-groups
-#             pick = np.random.choice(matches,size=1, replace=False)
-#             unmatch = np.random.choice(grouped_labels[random_numbers[i+batch_size]],size=1, replace=False)
-#             s1.append(np.eye(21)[encoded_seqs[pick[0]]])
-#             s2.append(np.eye(21)[encoded_seqs[unmatch[0]]])
-#
-#             targets.append(0)
-#
-#     return [np.array(s1), np.array(s2)], np.array(targets)
+
 
 
 def get_batch(grouped_labels,encoded_seqs,batch_size,s="train"):
@@ -135,7 +101,7 @@ np.random.seed(2) #Set random seed - ensures same split every time
 #Onehot encode sequences
 sequences = np.array(sequence_df['sequence'])
 encoded_seqs = one_hot(sequences)
-filters =20
+
 #Too large to save
 
 
@@ -151,32 +117,32 @@ t2 = time.time()
 print('Formatted in', np.round(t2-t1,2),'seconds')
 
 #Tensorboard for logging and visualization
-#log_name = str(time.time())
-#tensorboard = TensorBoard(log_dir=out_dir+log_name)
+log_name = str(time.time())
+tensorboard = TensorBoard(log_dir=outdir+log_name)
 
 ######MODEL######
 #Parameters
 #net_params = read_net_params(params_file)
 
 #Variable params
-num_epochs=30
+num_epochs=200
 batch_size = 64 #int(net_params['batch_size'])
 kernel_size = 21
+filters =20
 input_dim = (600,21)
 num_res_blocks=1
 dilation_rate = 5
 seq_length=600
 #MODEL
 in_1 = keras.Input(shape = [600,21])
-#in_2 = keras.Input(shape = [600,21])
+
 
 def resnet(x, num_res_blocks):
 	"""Builds a resnet with 1D convolutions of the defined depth.
 	"""
 
 
-    	# Instantiate the stack of residual units
-    	#Similar to ProtCNN, but they used batch_size = 64, 2000 filters and kernel size of 21
+	# Instantiate the stack of residual units
 	for res_block in range(num_res_blocks):
 		batch_out1 = BatchNormalization()(x) #Bacth normalize, focus on segment
 		activation1 = Activation('relu')(batch_out1)
@@ -188,28 +154,18 @@ def resnet(x, num_res_blocks):
 		x = Conv1D(filters = int(filters/2), kernel_size = kernel_size, dilation_rate = 1, input_shape=input_dim, padding ="same")(x)
 		x = add([x, conv_out2]) #Skip connection
 
-
-
 	return x
 
 #Initial convolution
 in_1_conv = Conv1D(filters = filters, kernel_size = kernel_size, dilation_rate = 2, input_shape=input_dim, padding ="same")(in_1)
-#in_2_conv = Conv1D(filters = filters, kernel_size = kernel_size, dilation_rate = 2, input_shape=input_dim, padding ="same")(in_2)
 #Output (batch, steps(len), filters), filters = channels in next
 x1 = resnet(in_1_conv, num_res_blocks)
-#x2 = resnet(in_2_conv, num_res_blocks)
+
 
 #Maxpool along sequence axis
 maxpool1 = MaxPooling1D(pool_size=seq_length)(x1)
-#maxpool2 = MaxPooling1D(pool_size=seq_length)(x2)
 
 flat1 = Flatten()(maxpool1)  #Flatten
-#flat2 = Flatten()(maxpool2)  #Flatten
-
-#Should have sum of two losses:
-# Add a customized layer to compute the absolute difference between the encodings
-#L1_layer = Lambda(lambda tensors:abs(tensors[0] - tensors[1]))
-#L1_distance = L1_layer([flat1, flat2])
 
 probabilities = Dense(len(grouped_labels), activation='softmax')(flat1)
 
